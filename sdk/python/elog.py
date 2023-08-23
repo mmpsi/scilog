1f98c5db2bfe6bedbf8766f52e5501804e3cdb75
matthias muntwiler <matthias.muntwiler@psi.ch>
2023-08-23 18:12:33 +0200

    bugfixes and tests on qa

--- sdk/python/elog.py
+++ sdk/python/elog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 """
-elog command-line interface to scilog
+Command-line interface to SciLog.
+
+This script provides an Elog-like command line interface to SciLog
+so that we have to minimally modify our ingestion scripts.
+
+The interface is not meant to be compatible with Elog!
+Some of the options will have no effect, others are mandatory or have special meanings.
 """
 
 import argparse
@@ -9,62 +15,48 @@ import datetime
 from pathlib import Path
 import urllib3
 
-from scilog import Basesnippet, Paragraph, SciLog
+from scilog import Basesnippet, Paragraph, SciLog, LogbookMessage
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-"""
-/// elog -h <hostname> [-p port] [-d subdir]
-///                              Location where elogd is running
-///     -l logbook               Name of logbook
-///     -s                       Use SSL for communication
-///     [-v]                     For verbose output
-///     [-w password]            Write password defined on server
-///     [-u username password]   User name and password
-///     [-f <attachment>]        Up to 50 attachments
-///     -a <attribute>=<value>   Up to 50 attributes
-///     [-r <id>]                Reply to existing message
-///     [-q]                     Quote original text on reply
-///     [-e <id>]                Edit existing message
-///     [-x]                     Suppress email notification
-///     [-n 0|1|2]               Encoding: 0:ELcode,1:plain,2:HTML
-///     -m <textfile>] | <text>
-
-"""
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, fromfile_prefix_chars='@')
-    parser.add_argument("-h", "--host",
+    parser.add_argument("--host",
                         help="Server address")
-    parser.add_argument("-d", "--dir",
-                        help="Subdirectory")
     parser.add_argument("-p", "--port", type=int,
                         help="Port")
-    parser.add_argument("-s", "--ssl", action='store_true',
-                        help="Use SSL")
     parser.add_argument("-l", "--logbook",
-                        help="Name of logbook")
+                        help="Name (title) of logbook. "
+                             "This and the p-group attribute are used to select the logbook. "
+                             "If the p-group owns only one logbook, this argument is not necessary.")
     parser.add_argument("-v", "--verbose", action='store_true',
                         help="Verbose output")
-    parser.add_argument("-w", "--password",
-                        help="Password")
     parser.add_argument("-u", "--user",
-                        help="User name and password")
+                        help="User name.")
+    parser.add_argument("-w", "--password",
+                        help="Password.")
+    parser.add_argument("-g", "--pgroup",
+                        help="p-group like 'p12345'.")
     parser.add_argument("-f", "--attach", type=Path, action='append',
-                        help="Attachment file (up to 50)")
-    parser.add_argument("-a", "--attribute", action='append',
-                        help="Attribute (up to 50). Format: attribute=value.")
-    parser.add_argument("-r", "--reply-to", type=int,
-                        help="Reply to ID")
-    parser.add_argument("-q", "--quote", action='store_true',
-                        help="Quote original text on reply")
+                        help="Attachment file. "
+                             "Option can be repeated without limit.")
+    parser.add_argument("-t", "--tag", action='append',
+                        help="Tag. "
+                             "Option can be repeated without limit.")
+    parser.add_argument("-r", "--reply-to",
+                        help="Reply to ID. Not supported.")
     parser.add_argument("-e", "--edit", type=int,
-                        help="Edit existing message")
-    parser.add_argument("-x", "--no-notify", action='store_true',
-                        help="Suppress email notification")
-    parser.add_argument("-n", "--encoding", type=int, choices=[0, 1, 2],
-                        help="Encoding")
+                        help="Edit existing message. Not supported.")
+    parser.add_argument("-n", "--markup", type=int, choices=[0, 1, 2], default=1,
+                        help="Markup style: 1:plain, 2:HTML. "
+                             "Note: 0:ELcode is not supported. Text is treated as plain.")
+    parser.add_argument("-c", "--encoding", default="utf8",
+                        help="Character encoding of message file (-m option). "
+                             "All values supported by the Python open command are allowed. "
+                             "utf8 is recommended for all new applications. "
+                             "Other options: latin1, ascii, cp1252, macroman. "
+                             "This option is not supported by the original elog interface.")
     parser.add_argument("-m", "--message-file", type=Path,
                         help="Message file")
     parser.add_argument("message", nargs="?",
@@ -72,9 +64,7 @@ def parse_args() -> argparse.Namespace:
 
     clargs = parser.parse_args()
 
-    clargs.attribute = dict(attr.strip().split("=") for attr in clargs.attribute)
-    clargs.pgroup = clargs.attribute['p-group']
-    clargs.url = f"http{'s' if clargs.ssl}://{clargs.host}{':' + str(clargs.port) if clargs.port else ''}/api/v1"
+    clargs.url = f"https://{clargs.host}{':' + str(clargs.port) if clargs.port else ''}/api/v1"
 
     return clargs
 
@@ -82,29 +72,52 @@ def parse_args() -> argparse.Namespace:
 def main():
     clargs = parse_args()
 
-    tmp = Basesnippet()
-    tmp.id = "2"
+    options = {"username": clargs.user, 
+               "password": clargs.password}
+               
+    log = SciLog(clargs.url, options=options)
 
-    log = SciLog(clargs.url, options={"username": "gac-x03da@psi.ch"})
-    # print(log.token)
-    logbooks = log.get_logbooks(ownerGroup=clargs.pgroup)
-    print(logbooks)
+    logbook_args = {"ownerGroup": clargs.pgroup}
+    if clargs.logbook:
+        logbook_args["name"] = clargs.logbook
 
-    assert len(logbooks) == 1
+    logbooks = log.get_logbooks(**logbook_args)
+    assert len(logbooks) >= 1, "no logbooks match filter criteria"
+    assert len(logbooks) <= 1, "multiple logbooks match filter criteria"
     logbook = logbooks[0]
-    print(logbook)
-
     log.select_logbook(logbook)
 
-    begin_time = datetime.datetime.now()
-    for ii in range(1000):
-        log.send_message(f"<p>from python; number: {ii}</p>")
-    # print(res)
-    print(datetime.datetime.now() - begin_time)
+    msg = LogbookMessage()
 
-    # snips = log.get_snippets(snippetType="paragraph", ownerGroup=pgroup)
-    # print(snips)
+    if clargs.message:
+        msg.add_text(clargs.message)
 
+    if clargs.message_file:
+        with open(clargs.message_file, "rt", encoding=clargs.encoding) as f:
+            message_text = f.readlines()
+            if clargs.markup == 2:
+                message_text = "".join(message_text)
+            else:
+                message_text = "<p>" + "<br>".join(message_text) + "</p>"
+            msg.add_text(message_text)
+
+    try:
+        for att in clargs.attach:
+            msg.add_file(att)
+    except (AttributeError, TypeError):
+        pass
+
+    try:
+        for tag in clargs.tag:
+            msg.add_tag(tag)
+    except (AttributeError, TypeError):
+        pass
+
+    if clargs.reply_to:
+        msg.add_tag(f"reply-to:{clargs.reply_to}")
+
+    log.send_logbook_message(msg)
+    
 
 if __name__ == "__main__":
     main()
